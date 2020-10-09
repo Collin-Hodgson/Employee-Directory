b20fc1926d88e12d60eb55345cd73196e87626a0
Collin <collinhodgs@gmail.com>
2020-10-08 19:23:29 -0600

    Made a header folder in components and make a header

---
+++ employee/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div class="card">
+      <div class="card-header">Employee Directory</div>
+      <div class="card-body">
+        <h4 class="card-title">Employee Directory</h4>
+        <p class="card-text">
+          This application will show current employees and allows you to search
+          for employees by name.
+        </p>
+      </div>
+    </div>
+  );
+}
