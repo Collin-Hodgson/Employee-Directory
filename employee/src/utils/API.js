46c3f567649b04635a1b89a39db5fc3d41499ccf
Collin <collinhodgs@gmail.com>
2020-10-08 18:18:35 -0600

    Changed API to only pull 25 random employees

--- employee/src/utils/API.js
+++ employee/src/utils/API.js
@@ -3,10 +3,7 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomEmployee: function () {
-    return axios.get("https://randomuser.me/api/");
-  },
-  getEmployee: function (breed) {
-    return axios.get("https://randomuser.me/api/?inc=name");
+  getRandomEmployees: function () {
+    return axios.get("https://randomuser.me/api/?results=25");
   },
 };
