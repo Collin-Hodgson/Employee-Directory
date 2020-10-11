d49008264f995560ebb856d0f866f3df5416ed17
Collin <collinhodgs@gmail.com>
2020-10-10 20:04:55 -0600

    Finished table in SearchResults component

--- employee/src/components/SearchResults/index.js
+++ employee/src/components/SearchResults/index.js
@@ -7,24 +7,31 @@ function SearchResults(props) {
       <thead class="thead-dark">
         <tr>
           <th scope="col">#</th>
-          <th scope="col">Employee Image</th>
+          <th scope="col">Image</th>
           <th scope="col">Last Name</th>
           <th scope="col">First Name</th>
-          <th scope="col">Cell Phone</th>
-          <th scope="col">Email</th>
+          <th scope="col">
+            Cell <i class="fas fa-phone"></i>
+          </th>
+          <th scope="col">
+            Email <i class="far fa-envelope"></i>
+          </th>
         </tr>
       </thead>
+
       <tbody>
-        <tr>
-          <td scope="row"></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td scope="row"></td>
-          <td></td>
-          <td></td>
-        </tr>
+        {props.results.map((employee, index) => (
+          <tr key={employee.name.last}>
+            <th scope="row"></th>
+            <td>
+              <img src={employee.picture.thumbnail} alt="Employee Image"></img>{" "}
+            </td>
+            <td> {employee.name.last} </td>
+            <td> {employee.name.first} </td>
+            <td> {employee.cell} </td>
+            <td> {employee.email} </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
