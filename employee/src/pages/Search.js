0c4da3e42016f77d10870c3c617b53698c2951fc
Collin <collinhodgs@gmail.com>
2020-10-10 20:16:57 -0600

    Changed API to get employee list

--- employee/src/pages/Search.js
+++ employee/src/pages/Search.js
@@ -1,39 +1,37 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
 
 class Search extends Component {
   state = {
     search: "",
     breeds: [],
     results: [],
-    error: ""
+    error: "",
   };
 
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
   componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
+    API.getEmployeeList()
+      .then((res) => this.setState({ employees: res.data.results }))
+      .catch((err) => console.log(err));
   }
 
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     this.setState({ search: event.target.value });
   };
 
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
     API.getDogsOfBreed(this.state.search)
-      .then(res => {
+      .then((res) => {
         if (res.data.status === "error") {
           throw new Error(res.data.message);
         }
         this.setState({ results: res.data.message, error: "" });
       })
-      .catch(err => this.setState({ error: err.message }));
+      .catch((err) => this.setState({ error: err.message }));
   };
   render() {
     return (
