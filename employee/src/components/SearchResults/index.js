570a627e051db592e126d14b427618a644a0f38e
Collin <collinhodgs@gmail.com>
2020-10-10 19:11:27 -0600

    Added header for thead row for SearchResults

--- employee/src/components/SearchResults/index.js
+++ employee/src/components/SearchResults/index.js
@@ -4,11 +4,14 @@ import "./style.css";
 function SearchResults(props) {
   return (
     <table class="table table-striped table-inverse table-responsive">
-      <thead class="thead-inverse">
+      <thead class="thead-dark">
         <tr>
-          <th></th>
-          <th></th>
-          <th></th>
+          <th scope="col">#</th>
+          <th scope="col">Employee Image</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Cell Phone</th>
+          <th scope="col">Email</th>
         </tr>
       </thead>
       <tbody>
